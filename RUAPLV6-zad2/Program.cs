25bc7b6269ecc68c2f4cdf13dfe0961e5785bcdc
unknown <kristman.denis@yahoo.com>
2016-05-24 18:33:52 +0200

    Initial commit

---
+++ RUAPLV6-zad2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RUAPLV6_zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
